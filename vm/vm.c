5bbc6a8c0fa310fd736ef3cc3bd16a34521257d7
Ubuntu <ubuntu@ip-172-31-10-163.ap-northeast-2.compute.internal>
2024-10-12 04:15:51 +0000

    feat✨: vm_do_claim_page() 추가

--- vm/vm.c
+++ vm/vm.c
@@ -217,7 +217,7 @@ vm_do_claim_page (struct page *page) {
 	page->frame = frame;
 
 	/* TODO: Insert page table entry to map page's VA to frame's PA. */
-		
+	pml4_set_page(thread_current()->pml4, page->va, frame->kva, true);
 
 	return swap_in (page, frame->kva);
 }
